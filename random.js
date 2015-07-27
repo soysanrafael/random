1be982cbd3fefa7352b5f3501c333fa6286ac43c
soysanrafael <soysanrafael@gmail.com>
2015-07-27 20:53:38 +0200

    random version 1

---
+++ random.js
@@ -0,0 +1,8 @@
+//Math.random() devuelve numero aleatorio entre 0 y 1
+var numero = Math.random();
+
+if(numero <= 0.5){
+  console.log('\n'+numero+' MENOR QUE 0,5 \n');
+}else {
+  console.log('\n'+numero+' MAYOR QUE 0,5 \n');
+}
